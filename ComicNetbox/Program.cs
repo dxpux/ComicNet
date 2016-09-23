f99c3b67c520edb95373a2add0500b9609765b5c
力及 <dxpux@hotmail.com>
2016-09-23 14:43:36 +0800

    project initial

---
+++ ComicNetbox/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Hosting;
+using Nancy;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicNetbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<MySartup>("http://+:8080"))
+            {
+                Console.WriteLine("OWIN hosting, press enter to exit");
+                Console.Read();
+            }
+        }
+    }
+
+    public class MySartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
