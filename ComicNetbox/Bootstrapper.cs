b8528cd12591144eefd0dd264eb81c05266932a2
力及 <dxpux@hotmail.com>
2016-09-24 15:45:56 +0800

    show all jpg files of the first folder which under comicPath 秀出 comicPath 底下第一個資料夾的所有 jpg 檔

---
+++ ComicNetbox/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Conventions;
+
+namespace ComicNetbox
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Clear();
+
+            //set comic path
+            string comicPath = ConfigurationManager.AppSettings["comicPath"].ToString();
+            nancyConventions.StaticContentsConventions.Add
+                (StaticContentConventionBuilder.AddDirectory("comic", comicPath));
+            nancyConventions.StaticContentsConventions.Add
+                (StaticContentConventionBuilder.AddDirectory("js", "/Scripts"));
+        }
+    }
+}
--- ComicNetbox/Modules/HomeModule.cs
+++ ComicNetbox/Modules/HomeModule.cs
@@ -1,18 +1,33 @@
 ﻿using Nancy;
 using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Web;
 
 namespace ComicNetbox.Modules
 {
     public class HomeModule : NancyModule
     {
+        private string rootPath;
+
         public HomeModule()
         {
+            // C:\Test\
+            rootPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // C:\Test\comicPath
+            string targetRoot = rootPath + ConfigurationManager.AppSettings["comicPath"].ToString().TrimStart('/').Replace('/', '\\');
+            DirectoryInfo di = new DirectoryInfo(targetRoot);
+
+            // C:\Test\comicPath\first\
+            var firstFolder = di.GetDirectories()[0];
+            var pictures = firstFolder.GetFiles("*.jpg").ToList();
+
+            // ["first/1.jpg", "first/2.jpg]
+            var picUrls = pictures.Select(pic => { return pic.FullName.Substring(rootPath.Length).Replace('\\', '/'); }).ToList();
             Get["/"] = parameters =>
             {
-                return View["/Home"];
+                return View["/Home", picUrls];
             };
         }
     }
