1c6e0b491b4a737884ee4682f81de9c292e88dfc
力及 <dxpux@hotmail.com>
2016-09-26 16:26:11 +0800

    using url to define the comic page target to display pictures 用 url 來定義要顯示的圖片資料夾

--- ComicNetbox/Modules/HomeModule.cs
+++ ComicNetbox/Modules/HomeModule.cs
@@ -8,25 +8,57 @@ namespace ComicNetbox.Modules
 {
     public class HomeModule : NancyModule
     {
-        private string rootPath;
+        private string rootPath { get { return AppDomain.CurrentDomain.BaseDirectory; } }
 
         public HomeModule()
         {
-            // C:\Test\
-            rootPath = AppDomain.CurrentDomain.BaseDirectory;
+            //TODO Get["/"] show dir and click to show comic page
 
-            // C:\Test\comicPath
-            string targetRoot = rootPath + ConfigurationManager.AppSettings["comicPath"].ToString().TrimStart('/').Replace('/', '\\');
-            DirectoryInfo di = new DirectoryInfo(targetRoot);
+            //Show comic page with path
+            ShowPage();
 
-            // C:\Test\comicPath\first\
-            var firstFolder = di.GetDirectories()[0];
-            var pictures = firstFolder.GetFiles("*.jpg").ToList();
+            //Test
+            ShowPageTest();
+        }
 
-            // ["first/1.jpg", "first/2.jpg]
-            var picUrls = pictures.Select(pic => { return pic.FullName.Substring(rootPath.Length).Replace('\\', '/'); }).ToList();
-            Get["/"] = parameters =>
+        private void ShowPage()
+        {
+            // url:~/comic?path=
+            Get["/comicpage/{path*}"] = x =>
             {
+                //string path = this.Request.Query["path"];
+                string path = x.path;
+                string comicDir = ConfigurationManager.AppSettings["comicPath"].ToString().TrimStart('/').Replace('/', '\\');
+                // C:\Test\comicPath\{path}
+                string targetRoot = string.Format(@"{0}{1}\{2}", rootPath, comicDir, path.Replace('/', '\\'));
+
+                DirectoryInfo di = new DirectoryInfo(targetRoot);
+
+                // C:\Test\comicPath\first\
+                var pictures = di.GetFiles("*.jpg").ToList();
+
+                // ["first/1.jpg", "first/2.jpg]
+                var picUrls = pictures.Select(pic => { return pic.FullName.Substring(rootPath.Length).Replace('\\', '/'); }).ToList();
+
+                return View["/ComicPage", picUrls];
+            };
+        }
+
+        private void ShowPageTest()
+        {
+            Get["/Test"] = parameters =>
+            {
+                // C:\Test\comicPath
+                string targetRoot = rootPath + ConfigurationManager.AppSettings["comicPath"].ToString().TrimStart('/').Replace('/', '\\');
+                DirectoryInfo di = new DirectoryInfo(targetRoot);
+
+                // C:\Test\comicPath\first\
+                var firstFolder = di.GetDirectories()[0];
+                var pictures = firstFolder.GetFiles("*.jpg").ToList();
+
+                // ["first/1.jpg", "first/2.jpg]
+                var picUrls = pictures.Select(pic => { return pic.FullName.Substring(rootPath.Length).Replace('\\', '/'); }).ToList();
+
                 return View["/Home", picUrls];
             };
         }
